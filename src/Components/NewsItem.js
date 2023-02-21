095e2a4b9ebe766c41e4e2dc1c244e24cff3fde3
Prajwal-ak-0 <prajwalhassan7@gmail.com>
2023-02-21 09:39:57 +0530

    Optimised

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -44,16 +44,6 @@ export class NewsItem extends Component {
     this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-  };
-
-  handleNextClick = async () => {
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-  };
-
   fetchMoreData =async () => {
     this.setState({page: this.state.page+1})
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c084ee68265f4e35ab3019fdbae1ffc0&page=${this.state.page}&pageSize=${this.props.pageSize}`;
@@ -101,64 +91,9 @@ export class NewsItem extends Component {
         </div>
         </div>
         </InfiniteScroll>
-
-        <div className="container my-4 d-flex justify-content-around">
-          <button
-            disabled={this.state.page <= 1}
-            type="button "
-            className="btn btn-dark"
-            onClick={this.handlePrevClick}
-          >
-            &larr;Previous
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
-            Next&rarr;
-          </button>
-        </div>
       </>
     );
   }
 }
 
 export default NewsItem;
-
-//1 Did Mount
-// let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c084ee68265f4e35ab3019fdbae1ffc0&page=1&pageSize=${this.props.pageSize}`;
-// let data=await fetch(url);
-// let parsedData=await data.json();
-// this.setState({articles: parsedData.articles,totalResults: parsedData.totalResults})
-// console.log(parsedData);
-
-//2Prev Click
-// let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c084ee68265f4e35ab3019fdbae1ffc0&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-// this.setState({loading:true});
-// let data=await fetch(url);
-// let parsedData=await data.json();
-// this.setState({
-//   page:this.state.page-1,
-//   articles: parsedData.articles,
-//   loading:false
-// })
-
-//3Next Click
-// if(!(this.state.page + 1 >Math.ceil(this.state.totalResults/this.props.pageSize)))
-// {
-//   // let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c084ee68265f4e35ab3019fdbae1ffc0&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-//   // this.setState({loading:true});
-//   // let data=await fetch(url);
-//   // let parsedData=await data.json();
-//   // this.setState({
-//   //   articles: parsedData.articles,
-//   //   page:this.state.page+1,
-//   //   loading:false
-//   // })
-
-// }
